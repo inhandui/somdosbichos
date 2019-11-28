465b8c4542a54f01f80cbfc8f73c8b6b4a2a5ee9
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-27 23:22:33 -0300

    Adding method to play the animal sound (tocarSom).

--- app/src/main/java/com/cursoandroid/somdosbichos/MainActivity.java
+++ app/src/main/java/com/cursoandroid/somdosbichos/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import static com.cursoandroid.somdosbichos.R.raw.*;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView cao;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView ovelha;
     private ImageView vaca;
 
-
+    private MediaPlayer mplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +44,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.cao:
+                mplayer = MediaPlayer.create(MainActivity.this, R.raw.cao);
+                tocarSom();
+                break;
+            case R.id.gato:
+                mplayer = MediaPlayer.create(MainActivity.this, R.raw.gato);
+                tocarSom();
+                break;
+            case R.id.leao:
+                mplayer = MediaPlayer.create(MainActivity.this, R.raw.leao);
+                tocarSom();
+                break;
+            case R.id.macaco:
+                mplayer = MediaPlayer.create(MainActivity.this, R.raw.macaco);
+                tocarSom();
+                break;
+            case R.id.ovelha:
+                mplayer = MediaPlayer.create(MainActivity.this, R.raw.ovelha);
+                tocarSom();
+                break;
+            case R.id.vaca:
+                mplayer = MediaPlayer.create(MainActivity.this, R.raw.vaca);
+                tocarSom();
+                break;
+        }
+
+    }
+
+    private void tocarSom() {
+        if (mplayer != null){
+            mplayer.start();
+        }
     }
 }
